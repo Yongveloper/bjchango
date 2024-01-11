3107846ed3b49f469d40f748a2c01b4cbbdf6e09
Yongveloper <aaa9015@naver.com>
2024-01-11 18:53:07 +0900

    feat: RTK-query 데이터 받아오기 RTK을 사용함으로써 내장되어 있는 RTK-query를 사용해서 서버 데이터 받아오기

---
+++ src/api/productsApi.ts
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+interface IProduct {
+  id: string;
+  name: string;
+  event: number;
+  materialType: number;
+  price: number;
+}
+
+const BASE_URL = 'http://localhost:3001/';
+
+export const productsApi = createApi({
+  reducerPath: 'productsApi',
+  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
+  endpoints: (builder) => ({
+    getProducts: builder.query<IProduct[], void>({
+      query: () => 'items',
+    }),
+  }),
+});
+
+export const { useGetProductsQuery } = productsApi;
--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { productsApi } from 'api/productsApi';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    [productsApi.reducerPath]: productsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(productsApi.middleware),
   devTools: process.env.NODE_ENV !== 'production',
 });
 
