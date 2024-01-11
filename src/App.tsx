1fa0eee976b1a7781fdc304aec143ac12d6e54cc
Yongveloper <aaa9015@naver.com>
2024-01-11 15:15:56 +0900

    chore: react-router-dom 설치 및 설정 페이지 라우팅을 위한 react-router-dom 설치와 초기 페이지 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from 'pages/Home';
+import Order from 'pages/Order';
+import Complete from 'pages/Complete';
+import Error from 'pages/Error';
+
 function App() {
-  return <div className="App">home</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/order" element={<Order />} />
+        <Route path="/complete" element={<Complete />} />
+        <Route path="/error" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
