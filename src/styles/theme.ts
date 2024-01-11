70b7a1e4fb356323b4e1279293cd20fbfe59cb39
Yongveloper <aaa9015@naver.com>
2024-01-11 14:09:26 +0900

    chore: styled-components 설치 및 설정 styled-components를 설치하고 초기 구조 설정

---
+++ src/styles/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from 'styled-components';
+
+export const theme: DefaultTheme = {
+  colors: {
+    white: '#fff',
+    black: '#000',
+    orange: '#F75A2F',
+    grey: '#D9D9D9',
+    button: {
+      disabled: '#C1C1C1',
+    },
+  },
+};
