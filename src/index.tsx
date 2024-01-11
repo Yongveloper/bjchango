d381f0d7bfe01bb458c4b239206d7dc27e565d62
Yongveloper <aaa9015@naver.com>
2024-01-11 15:02:14 +0900

    chore: 절대경로 설정 상대경로로 가독성이 떨어지는 상황을 방지

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import { store } from './store';
 import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
-import { GlobalStyles } from './styles/GlobalStyles';
+import { GlobalStyles } from 'styles/GlobalStyles';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
