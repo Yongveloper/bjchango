2f9ee5a49886e46c9983e18543b7418cc333cfcf
Yongveloper <aaa9015@naver.com>
2024-01-11 18:33:20 +0900

    feat: Header top 추가 명확하게 어디 포지션인지 추가함

--- src/pages/Order/Order.styles.ts
+++ src/pages/Order/Order.styles.ts
@@ -12,6 +12,7 @@ export const Header = styled.header`
   width: 100%;
   max-width: ${({ theme }) => theme.device.mobile};
   position: fixed;
+  top: 0;
   height: 57px;
   padding: 12px;
   background-color: ${({ theme }) => theme.colors.black};
