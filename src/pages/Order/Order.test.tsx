cf6f52062cc87f75146f374b38759b5579a10b8f
Yongveloper <aaa9015@naver.com>
2024-01-12 20:35:10 +0900

    test: 데이터 받아오면 제품 렌더링 테스트

--- src/pages/Order/Order.test.tsx
+++ src/pages/Order/Order.test.tsx
@@ -1,4 +1,4 @@
-import { screen } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
 import Order from './Order';
 import { renderWithProviders } from '__test__/renderWithProviders';
 import fetchMock from 'jest-fetch-mock';
@@ -18,14 +18,14 @@ describe('Order 컴포넌트 테스트', () => {
     fetchMock.resetMocks();
   });
 
-  it('Header가 정상적으로 렌더링 되어야 함', () => {
+  test('Header가 정상적으로 렌더링 되어야 함', () => {
     fetchMock.mockResponseOnce(JSON.stringify(mockData));
     renderWithProviders(<Order />);
     const logoElement = screen.getByAltText('Logo');
     expect(logoElement).toBeInTheDocument();
   });
 
-  it('로딩 상태일 때 로딩 상태가 표시 되어야 함', async () => {
+  test('로딩 상태일 때 로딩 상태가 표시 되어야 함', async () => {
     fetchMock.mockResponseOnce(
       () =>
         new Promise((resolve) =>
@@ -39,11 +39,22 @@ describe('Order 컴포넌트 테스트', () => {
     expect(await screen.findByText(/불러오고 있습니다./)).toBeInTheDocument();
   });
 
-  it('에러 발생 시 에러 상태가 표시 되어야 함', async () => {
+  test('에러 발생 시 에러 상태가 표시 되어야 함', async () => {
     fetchMock.mockReject(() => Promise.reject('Error'));
     renderWithProviders(<Order />);
 
     expect(await screen.findByText(/목록을/)).toBeInTheDocument();
     expect(await screen.findByText(/불러오지 못했습니다./)).toBeInTheDocument();
+  });
+
+  test('정상적으로 데이터를 받아오면 제품을 표시해야 함', async () => {
+    fetchMock.mockResponseOnce(JSON.stringify(mockData));
+    renderWithProviders(<Order />);
+
+    const productCardElement = await waitFor(() =>
+      screen.getByText('product1')
+    );
+
+    expect(productCardElement).toBeInTheDocument();
   });
 });
