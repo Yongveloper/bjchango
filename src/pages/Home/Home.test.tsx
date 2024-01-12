5afb0920696cb4939ddbebb316c99d12fc1ec90e
Yongveloper <aaa9015@naver.com>
2024-01-12 13:12:07 +0900

    test: Home 컴포넌트 로고 이미지 렌더링 테스트 코드 추가

--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, screen } from '@testing-library/dom';
+import { screen } from '@testing-library/dom';
 import Home from './Home';
 import renderWithProvider from '__test__/renderWithProviders';
 
@@ -9,14 +9,5 @@ describe('<Home />', () => {
     const logoImage = screen.getByAltText('logo');
 
     expect(logoImage).toBeInTheDocument();
-  });
-
-  test("'주문하러 가기' 버튼을 클릭하면 '/order' 경로로 이동하는지 확인", () => {
-    const { getByText } = renderWithProvider(<Home />);
-
-    const orderButton = getByText('주문하러 가기');
-    fireEvent.click(orderButton);
-
-    expect(window.location.pathname).toBe('/order');
   });
 });
