1c7e7e878f9fa5c0ebfe3ee1ddd8dd54a9c60660
Yongveloper <aaa9015@naver.com>
2024-01-12 15:22:38 +0900

    test: productApi 테스트

---
+++ src/api/productsApi.test.tsx
@@ -0,0 +1,30 @@
+import { renderHook, waitFor } from '@testing-library/react';
+import { useGetProductsQuery } from './productsApi';
+import fetchMock from 'jest-fetch-mock';
+import { store } from 'store';
+import { Provider } from 'react-redux';
+
+function Wrapper({ children }: { children: React.ReactNode }) {
+  return <Provider store={store}>{children}</Provider>;
+}
+
+beforeEach(() => {
+  fetchMock.resetMocks();
+});
+
+describe('productsApi 테스트', () => {
+  it('제품 정보를 정확히 불러와야 함', async () => {
+    const data = [
+      { id: '1', name: 'Product 1', event: 1, materialType: 1, price: 100 },
+    ];
+    fetchMock.mockResponseOnce(JSON.stringify(data));
+
+    const { result } = renderHook(() => useGetProductsQuery(), {
+      wrapper: Wrapper,
+    });
+
+    waitFor(() => {
+      expect(result.current.data).toEqual(data);
+    });
+  });
+});
