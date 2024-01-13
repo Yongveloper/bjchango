a63e2f2b8bcf0d642a3b4911a363ad70ae113629
Yongveloper <aaa9015@naver.com>
2024-01-13 17:37:52 +0900

    test: 해당 상품이 있으면 수량 증가하는 테스트 코드 추가

--- src/slices/cartSlice.test.ts
+++ src/slices/cartSlice.test.ts
@@ -15,7 +15,7 @@ describe('cartSlice 테스트', () => {
     expect(cartReducer(undefined, { type: 'unknown' })).toEqual(initialState);
   });
 
-  test('상품이 장바구니에 추가되어야 함', () => {
+  test('해당 상품이 장바구니에 없으면 장바구니에 상품이 추가되어야 함', () => {
     const product = {
       id: '1',
       name: 'product1',
@@ -30,6 +30,30 @@ describe('cartSlice 테스트', () => {
     };
 
     const result = cartReducer(initialState, addToCart(product));
+
+    expect(result).toEqual(expectedState);
+  });
+
+  test('해당 상품이 장바구니에 있으면 수량이 1 증가해야 함', () => {
+    const product = {
+      id: '1',
+      name: 'product1',
+      event: 0,
+      materialType: 1,
+      price: 100,
+    };
+    const state = {
+      items: { '1': { ...product, quantity: 1 } },
+      totalQuantity: 1,
+      totalPrice: 100,
+    };
+    const expectedState = {
+      items: { '1': { ...product, quantity: 2 } },
+      totalQuantity: 2,
+      totalPrice: 200,
+    };
+
+    const result = cartReducer(state, addToCart(product));
 
     expect(result).toEqual(expectedState);
   });
