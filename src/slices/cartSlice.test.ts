f4d969153d6845059919910d1325341e817b73a9
Yongveloper <aaa9015@naver.com>
2024-01-13 17:34:51 +0900

    test: 장바구니에 상품을 추가하는 테스트 코드 추가

--- src/slices/cartSlice.test.ts
+++ src/slices/cartSlice.test.ts
@@ -1,7 +1,7 @@
-import cartReducer from 'slices/cartSlice';
+import cartReducer, { ICartState, addToCart } from 'slices/cartSlice';
 
 describe('cartSlice 테스트', () => {
-  let initialState = {};
+  let initialState: ICartState;
 
   beforeEach(() => {
     initialState = {
@@ -13,5 +13,24 @@ describe('cartSlice 테스트', () => {
 
   test('초기 상태를 가지고 있어야 함', () => {
     expect(cartReducer(undefined, { type: 'unknown' })).toEqual(initialState);
+  });
+
+  test('상품이 장바구니에 추가되어야 함', () => {
+    const product = {
+      id: '1',
+      name: 'product1',
+      event: 0,
+      materialType: 1,
+      price: 100,
+    };
+    const expectedState = {
+      items: { '1': { ...product, quantity: 1 } },
+      totalQuantity: 1,
+      totalPrice: 100,
+    };
+
+    const result = cartReducer(initialState, addToCart(product));
+
+    expect(result).toEqual(expectedState);
   });
 });
