769711742f3ccf189d7753cd2194ecfd5cf85e5f
Yongveloper <aaa9015@naver.com>
2024-01-13 12:50:44 +0900

    test: 제품 이름 렌더링 테스트 코드 추가

--- src/components/ProductCard/ProductCard.test.tsx
+++ src/components/ProductCard/ProductCard.test.tsx
@@ -18,4 +18,20 @@ describe('<ProductCard />', () => {
 
     expect(productImg).toBeInTheDocument();
   });
+
+  test('제품 이름이 렌더링 되어야 함', () => {
+    renderWithProviders(
+      <ProductCard
+        id="1"
+        name="제품이름"
+        event={0}
+        materialType={1}
+        price={10000}
+      />
+    );
+
+    const productName = screen.getByText('제품이름');
+
+    expect(productName).toBeInTheDocument();
+  });
 });
