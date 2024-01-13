941205b6032d5d7cd43ea79ecc23e2cb6a9ae8a7
Yongveloper <aaa9015@naver.com>
2024-01-13 17:02:23 +0900

    test: Order 컴포넌트 렌더링 테스트 코드 추가

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from 'App';
+import { Provider } from 'react-redux';
 import { MemoryRouter } from 'react-router-dom';
+import { setupStore } from 'store';
 import { ThemeProvider } from 'styled-components';
 import { theme } from 'styles/theme';
+import fetchMock from 'jest-fetch-mock';
 
 describe('<App />', () => {
   test("'/'경로에서는 <Home /> 컴포넌트가 렌더링 되어야 함", () => {
@@ -18,5 +21,34 @@ describe('<App />', () => {
     const logo = screen.getByAltText('logo');
 
     expect(logo).toBeInTheDocument();
+  });
+
+  test("'/order'경로에서는 <Order /> 컴포넌트가 렌더링 되어야 함", () => {
+    const mockData = [
+      {
+        id: 1,
+        name: 'product1',
+        event: 'event1',
+        materialType: 'type1',
+        price: 100,
+      },
+    ];
+    fetchMock.mockResponseOnce(JSON.stringify(mockData));
+    const route = '/order';
+    const store = setupStore({});
+
+    render(
+      <MemoryRouter initialEntries={[route]}>
+        <Provider store={store}>
+          <ThemeProvider theme={theme}>
+            <App />
+          </ThemeProvider>
+        </Provider>
+      </MemoryRouter>
+    );
+
+    const headerLogo = screen.getByAltText('Logo');
+
+    expect(headerLogo).toBeInTheDocument();
   });
 });
