6968bee7b6e8a485675563ffee848be3c57dc965
Yongveloper <aaa9015@naver.com>
2024-01-13 15:45:29 +0900

    test: 페이지 진입 3초후 주문 페이지 이동 테스트 코드 추가

--- src/pages/Error/Error.test.tsx
+++ src/pages/Error/Error.test.tsx
@@ -1,6 +1,6 @@
 import { renderWithProviders } from '__test__/renderWithProviders';
 import Error from './Error';
-import { screen } from '@testing-library/dom';
+import { screen, waitFor } from '@testing-library/dom';
 
 describe('<Error />', () => {
   test('주문 실패한 텍스트가 렌더링 되어야 함', async () => {
@@ -10,5 +10,18 @@ describe('<Error />', () => {
       await screen.findByText(/주문에 실패하였습니다./)
     ).toBeInTheDocument();
     expect(await screen.findByText(/다시 시도해주세요./)).toBeInTheDocument();
+  });
+
+  test('페이지 진입 후 3초 뒤에 주문 페이지로 이동해야 함', async () => {
+    renderWithProviders(<Error />);
+
+    await waitFor(
+      () => {
+        expect(window.location.pathname).toBe('/order');
+      },
+      {
+        timeout: 3500,
+      }
+    );
   });
 });
