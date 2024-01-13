1d748a13c3fb78e5e69a09d50d91c0ba098958fb
Yongveloper <aaa9015@naver.com>
2024-01-13 15:39:03 +0900

    test: 3초 뒤 주문 페이지로 이동하는 테스트 코드 추가

--- src/pages/Complete/Complete.test.tsx
+++ src/pages/Complete/Complete.test.tsx
@@ -1,6 +1,6 @@
 import { renderWithProviders } from '__test__/renderWithProviders';
 import Complete from './Complete';
-import { screen } from '@testing-library/dom';
+import { screen, waitFor } from '@testing-library/dom';
 
 describe('<Complete />', () => {
   test('체크 아이콘 이미지가 렌더링 되어야 함', () => {
@@ -17,5 +17,18 @@ describe('<Complete />', () => {
     const completeElement = screen.getByText('주문이 완료되었습니다.');
 
     expect(completeElement).toBeInTheDocument();
+  });
+
+  test('페이지 진입 후 3초 뒤에 주문 페이지로 이동해야 함', async () => {
+    renderWithProviders(<Complete />);
+
+    await waitFor(
+      () => {
+        expect(window.location.pathname).toBe('/order');
+      },
+      {
+        timeout: 3500,
+      }
+    );
   });
 });
